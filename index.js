492592bf37c8aac0441df8248ce3fb0788ed6a5a
vamsi-1438 <chirunavu1438@gmail.com>
2022-08-11 11:07:13 +0000

    SUPEROVERLEAUGE

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="container">
+    <h1>Super Over League</h1>
+    <div className="container2">
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="img"
+        src="https://asset.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
